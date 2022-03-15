a7429a9a087a9182863fd2b60aad45a4805a34ee
Vinit-28 <codervinit@gmail.com>
2022-03-15 19:14:06 +0530

    Modified __init__.py File, Added Username and Password

--- __init__.py
+++ __init__.py
@@ -5,8 +5,8 @@ from urllib.parse import quote_plus
 
 
 # Making Connection to the Database #
-dbUserName = quote_plus("codervinit")
-dbPassword = quote_plus("####@@**")
+dbUserName = quote_plus("username")
+dbPassword = quote_plus("password")
 connectionString = ("mongodb+srv://%s:%s@thebookslibrary.elqki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" % (dbUserName, dbPassword))
 dbUtility = dbUtilities(connectionString = connectionString)
 dbName = "myApp"
